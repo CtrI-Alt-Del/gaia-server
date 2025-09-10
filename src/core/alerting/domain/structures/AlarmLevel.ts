e62a8746c16609e3fe5ab5623be469376681763b
CaiquePastelDePao <caiquepericles.23@gmail.com>
2025-09-10 10:40:24 -0300

    feat: logical methods and validationError handling in the create method in AlarmLevel Structure

--- src/core/alerting/domain/structures/AlarmLevel.ts
+++ src/core/alerting/domain/structures/AlarmLevel.ts
@@ -1,3 +1,6 @@
+import { ValidationException } from "@/core/global/domain/exceptions/validation-exception"
+import { Logical, Text } from "@/core/global/domain/structures"
+
 export enum Type{
     WARNING,
     CRITICAL
@@ -10,7 +13,16 @@ export default class AlarmLevel{
     }
 
     public static create(value: string): AlarmLevel{
-        return new AlarmLevel(Type[value.toLocaleUpperCase()])
+        if (value === "") {
+            throw new ValidationException("Nível de alarme", "não pode ser nulo")
+        }
+        const text = Text.create(value.toLocaleUpperCase())
+
+        try {
+            return new AlarmLevel(Type[text.value])   
+        } catch (error) {
+            throw new ValidationException("Nível de alarme", "com valor inválido")
+        }
     }
 
     public static createAsWarning(): AlarmLevel{
@@ -19,5 +31,17 @@ export default class AlarmLevel{
 
     public static createAsCritical(): AlarmLevel{
         return new AlarmLevel(Type.CRITICAL)
+    }
+
+    public isTypeWarning(): Logical{
+        return Logical.create(this.type === Type.WARNING)
+    }
+
+    public isTypeCritical(): Logical{
+        return Logical.create(this.type === Type.CRITICAL)
+    }
+
+    public toString(): string{
+        return this.type.toString().toLocaleLowerCase()
     }
 }

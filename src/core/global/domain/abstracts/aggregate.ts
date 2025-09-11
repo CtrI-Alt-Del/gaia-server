118f0995b034479fabd7a0255c4c1acf19bff64c
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-11 20:39:31 -0300

    refactor: enhance Entity and Aggregate classes with improved type handling and constructor logic

--- src/core/global/domain/abstracts/aggregate.ts
+++ src/core/global/domain/abstracts/aggregate.ts
@@ -1,31 +1,33 @@
-import { Entity } from "@/core/global/domain/abstracts/entity";
-import { Logical } from "@/core/global/domain/structures";
+import { EnityProps, Entity } from '@/core/global/domain/abstracts/entity'
+import { Logical } from '@/core/global/domain/structures'
 
-export abstract class Aggregate<Props, AggregableEntity> extends Entity<Props> {
-	private readonly entity: AggregableEntity;
-	private readonly entityName: string;
+export abstract class Aggregate<
+  Props extends EnityProps,
+  AggregableEntity,
+> extends Entity<Props> {
+  private readonly entity: AggregableEntity
+  private readonly entityName: string
 
-	protected constructor(
-		props: Props,
-		entity: AggregableEntity,
-		entityName: string,
-		id?: string,
-	) {
-		super(props, id);
-		this.entity = entity;
-		this.entityName = entityName;
-	}
+  protected constructor(
+    props: Props,
+    entity: AggregableEntity,
+    entityName: string,
+    id?: string,
+  ) {
+    super(props, id)
+    this.entity = entity
+    this.entityName = entityName
+  }
 
-	getEntity(): AggregableEntity {
-		if (this.hasEntity().isTrue()) {
-			throw new Error(this.entityName);
-		}
-		return this.entity;
-	}
+  getEntity(): AggregableEntity {
+    if (this.hasEntity().isTrue) {
+      throw new Error(this.entityName)
+    }
+    return this.entity
+  }
 
-	hasEntity(): Logical {
-		const isEntityNotNullOrUndefined =
-			this.entity !== null || this.entity !== undefined;
-		return Logical.create(isEntityNotNullOrUndefined);
-	}
+  hasEntity(): Logical {
+    const isEntityNotNullOrUndefined = this.entity !== null || this.entity !== undefined
+    return Logical.create(isEntityNotNullOrUndefined)
+  }
 }

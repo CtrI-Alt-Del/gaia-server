c50773869eec9c28f2c937600e6131e77bcf970a
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-11 21:10:56 -0300

    feat: update main application setup with EnvProvider, add MembershipRestModule

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,21 +1,24 @@
-
-import { EnvService } from '@/infra/env/env.service'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import { AppModule } from 'src/infra/app.module'
+import { EnvProvider } from './provision/env/env-provider'
+import { AppModule } from './app.module'
+import { RestExceptionsFilter } from './rest/filters/rest-exception.filter'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Gaia API')
-    .setDescription('The Gaia API description')
+    .setDescription('Gaia API description')
     .setVersion('1.0')
     .addTag('gaia')
     .build()
   const document = SwaggerModule.createDocument(app, swaggerConfig)
   SwaggerModule.setup('docs', app, document)
-  const configService = app.get(EnvService)
+
+  const configService = app.get(EnvProvider)
   const port = configService.get('PORT')
+
+  app.useGlobalFilters(new RestExceptionsFilter())
   await app.listen(port)
 }
 bootstrap()
--- src/infra/rest/rest.module.ts
+++ src/infra/rest/rest.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+
 import { TelemetryRestModule } from './telemetry/telemetry-rest.module'
+import { MembershipRestModule } from './membership/membership-rest.module'
 
 @Module({
-  imports: [TelemetryRestModule],
+  imports: [TelemetryRestModule, MembershipRestModule],
 })
 export class RestModule {}

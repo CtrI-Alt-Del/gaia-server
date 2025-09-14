fd689af2e715209618ff7a8d94318ebef8969fd5
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-14 09:58:56 -0300

    feat: implement ListUsersController for retrieving active users with pagination

--- src/infra/rest/membership/membership-rest.module.ts
+++ src/infra/rest/membership/membership-rest.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 
 import { DatabaseModule } from '@/infra/database/database.module'
 import { CreateUserController } from './users/create-user.controller'
+import { ListUsersController } from './users/list-users.controller'
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateUserController],
+  controllers: [CreateUserController, ListUsersController],
 })
 export class MembershipRestModule {}

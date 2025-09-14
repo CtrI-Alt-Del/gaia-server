9a14aad55f3287204102474fdecdf69069385aba
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-14 09:57:33 -0300

    feat: add ListUsersUseCase for retrieving users with cursor pagination

---
+++ src/core/membership/use-cases/list-users-use-case.ts
@@ -0,0 +1,28 @@
+import { UsersRepository } from '@/core/global/interfaces'
+import { CursorPaginationDto } from '@/core/global/domain/structures/dtos'
+import { Id, Logical } from '@/core/global/domain/structures'
+import { PlusInteger } from '@/core/global/domain/structures'
+import { UserDto } from '../domain/entities/dtos'
+
+type Request = {
+  nextCursor?: string
+  previousCursor?: string
+  pageSize: number
+  isActive: boolean
+}
+
+export class ListUsersUseCase {
+  constructor(private readonly repository: UsersRepository) {}
+
+  async execute(params: Request): Promise<CursorPaginationDto<UserDto>> {
+    const pagination = await this.repository.findMany({
+      nextCursor: params.nextCursor ? Id.create(params.nextCursor) : undefined,
+      previousCursor: params.previousCursor
+        ? Id.create(params.previousCursor)
+        : undefined,
+      pageSize: PlusInteger.create(params.pageSize),
+      isActive: Logical.create(params.isActive),
+    })
+    return pagination.map((user) => user.dto).dto
+  }
+}

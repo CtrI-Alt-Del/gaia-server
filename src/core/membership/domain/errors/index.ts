09f681206bc80cbbc99a3599f4fbf126fc2441ec
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-15 00:40:29 -0300

    feat: add UserNotFoundError class for handling user not found scenarios

--- src/core/membership/domain/errors/index.ts
+++ src/core/membership/domain/errors/index.ts
@@ -1,2 +1,3 @@
 export { EmailAlreadyInUseError } from './email-already-in-use-error'
 export { OwnerCreationNotAllowed } from './owner-creation-not-allowed-error'
+export { UserNotFoundError } from './user-not-found-error'

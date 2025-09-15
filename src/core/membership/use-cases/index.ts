fed42955504ed240ec924cf748929e5287d8e613
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-15 00:41:33 -0300

    feat: add UpdateUserUseCase for updating user details and handling email conflicts

--- src/core/membership/use-cases/index.ts
+++ src/core/membership/use-cases/index.ts
@@ -1,2 +1,3 @@
 export { CreateUserUseCase } from './create-user-use-case'
 export { ListUsersUseCase } from './list-users-use-case'
+export { UpdateUserUseCase } from './update-user-use-case'
---
+++ src/core/membership/use-cases/update-user-use-case.ts
@@ -0,0 +1,35 @@
+import { Id, Text } from '@/core/global/domain/structures'
+import { UseCase, UsersRepository } from '@/core/global/interfaces'
+import { UserDto } from '../domain/entities/dtos'
+import { User } from '../domain/entities'
+import { EmailAlreadyInUseError, UserNotFoundError } from '../domain/errors'
+
+type Request = UserDto
+type Response = UserDto
+
+export class UpdateUserUseCase implements UseCase<Request, Response> {
+  constructor(private readonly repository: UsersRepository) {}
+
+  async execute(userDto: UserDto): Promise<Response> {
+    const user = await this.findUserById(Id.create(userDto.id))
+    user.update(userDto)
+
+    if (user.email.value !== userDto.email) {
+      await this.findUserByEmail(user.email)
+    }
+
+    await this.repository.replace(user)
+    return user.dto
+  }
+
+  async findUserById(id: Id): Promise<User> {
+    const user = await this.repository.findById(id)
+    if (!user) throw new UserNotFoundError()
+    return user
+  }
+
+  async findUserByEmail(email: Text): Promise<void> {
+    const user = await this.repository.findByEmail(email)
+    if (user) throw new EmailAlreadyInUseError()
+  }
+}

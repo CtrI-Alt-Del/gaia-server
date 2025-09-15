f51ce18fbd147ee7ed7f0808d5a1f5e997f86eff
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-15 01:44:37 -0300

    feat: add ActivateUserController to handle user activation requests and update membership module

--- src/infra/rest/membership/membership-rest.module.ts
+++ src/infra/rest/membership/membership-rest.module.ts
@@ -4,9 +4,15 @@ import { DatabaseModule } from '@/infra/database/database.module'
 import { CreateUserController } from './users/create-user.controller'
 import { ListUsersController } from './users/list-users.controller'
 import { UpdateUserController } from './users/update-user.controller'
+import { ActivateUserController } from './users/activate-user.controller'
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateUserController, ListUsersController, UpdateUserController],
+  controllers: [
+    ActivateUserController,
+    CreateUserController,
+    ListUsersController,
+    UpdateUserController,
+  ],
 })
 export class MembershipRestModule {}

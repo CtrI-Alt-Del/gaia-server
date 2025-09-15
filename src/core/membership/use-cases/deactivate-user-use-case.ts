1900675354e128f9ae8e2f0049829a7e258a16fd
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-15 01:49:35 -0300

    feat: implement DeactivateUserUseCase for user deactivation logic and error handling

---
+++ src/core/membership/use-cases/deactivate-user-use-case.ts
@@ -0,0 +1,33 @@
+import { Id } from '@/core/global/domain/structures'
+import { UseCase, UsersRepository } from '@/core/global/interfaces'
+import { UserDto } from '../domain/entities/dtos'
+import { User } from '../domain/entities'
+import { UserNotFoundError, UserAlreadyDeactivatedError } from '../domain/errors'
+
+type Request = {
+  id: string
+}
+type Response = UserDto
+
+export class DeactivateUserUseCase implements UseCase<Request, Response> {
+  constructor(private readonly repository: UsersRepository) {}
+
+  async execute(userDto: Request): Promise<Response> {
+    const user = await this.findUserById(Id.create(userDto.id))
+
+    if (user.isActive.isFalse) {
+      throw new UserAlreadyDeactivatedError()
+    }
+
+    user.deactivate()
+
+    await this.repository.replace(user)
+    return user.dto
+  }
+
+  async findUserById(id: Id): Promise<User> {
+    const user = await this.repository.findById(id)
+    if (!user) throw new UserNotFoundError()
+    return user
+  }
+}

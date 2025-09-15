2fa3739c7c7df7cc38264aaed3054c1f53f4008f
CaiquePastelDePao <caiquepericles.23@gmail.com>
2025-09-15 08:02:51 -0300

    fix: changed alarm entity to contain parameter-aggregate, isactive, createdat and updatedat fields

--- src/core/alerting/domain/entities/alarm.ts
+++ src/core/alerting/domain/entities/alarm.ts
@@ -1,37 +1,57 @@
 import { Entity } from "@/core/global/domain/abstracts"
-import { Text } from "@/core/global/domain/structures"
-import { Measurement } from "@/core/telemetry/entities/measurement"
+import { Logical, Text, Timestamp } from "@/core/global/domain/structures"
 import { AlarmDto } from "../../dtos/alarm.dto"
+import { ParameterAggregate } from "../../aggregate/parameter-aggregate"
 
 type AlarmProps = {
-    name: Text
-    measure: Measurement
+    message: Text
+    parameter: ParameterAggregate
+    isActive: Logical
+    createdAt: Timestamp
+    updatedAt?: Timestamp
 }
 
 export class Alarm extends Entity<AlarmProps>{
     static create(dto: AlarmDto): Alarm{
         return new Alarm(
             {
-                name: Text.create(dto.name),
-                measure: Measurement.create(dto.measure)
+                message: Text.create(dto.message),
+                parameter: dto.parameter,
+                isActive: Logical.create(dto.isActive),
+                createdAt: Timestamp.create(dto.createdAt)
             },
             dto.id
         )
     }
 
-    get name(): Text{
-        return this.props.name
+    get message(): Text{
+        return this.props.message
     }
 
-    get measure(): Measurement{
-        return this.props.measure
+    get parameter(): ParameterAggregate {
+        return this.props.parameter
+    }
+
+    get isActive(): Logical{
+        return this.props.isActive
+    }
+
+    get createdAt(): Timestamp {
+        return this.props.createdAt
+    }
+
+    get updatedAt(): Timestamp | undefined{
+        return this.props.updatedAt
     }
 
     get dto(): AlarmDto{
         return {
             id: this.id.value,
-            name: this.name.value,
-            measure: this.measure.dto
+            message: this.message.value,
+            parameter: this.parameter,
+            isActive: this.isActive.value,
+            createdAt: this.createdAt.value,
+            updatedAt: this.updatedAt?.value
         }
     }
 }
--- src/core/alerting/dtos/alarm.dto.ts
+++ src/core/alerting/dtos/alarm.dto.ts
@@ -2,8 +2,9 @@ import { ParameterAggregate } from "../aggregate/parameter-aggregate"
 
 export type AlarmDto = {
     id: string
-    name: string
+    message: string
     parameter: ParameterAggregate
     isActive: boolean
     createdAt: Date
+    updatedAt?: Date
 }

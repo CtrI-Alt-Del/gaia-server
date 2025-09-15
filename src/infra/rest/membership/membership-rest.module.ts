d7e3fa992edb71756029ca23ab0beb5caaed8d0d
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-15 00:41:50 -0300

    feat: add UpdateUserController for updating user details in the membership module

--- src/infra/rest/membership/membership-rest.module.ts
+++ src/infra/rest/membership/membership-rest.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common'
 import { DatabaseModule } from '@/infra/database/database.module'
 import { CreateUserController } from './users/create-user.controller'
 import { ListUsersController } from './users/list-users.controller'
+import { UpdateUserController } from './users/update-user.controller'
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [CreateUserController, ListUsersController],
+  controllers: [CreateUserController, ListUsersController, UpdateUserController],
 })
 export class MembershipRestModule {}

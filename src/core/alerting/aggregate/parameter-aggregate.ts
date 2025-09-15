43dbf5a4aee52f1b857e6b8854ebd56353a1a3a3
CaiquePastelDePao <caiquepericles.23@gmail.com>
2025-09-15 08:32:45 -0300

    fix: changed parameteraggregate from type to a class aggregate

--- src/core/alerting/aggregate/parameter-aggregate.ts
+++ src/core/alerting/aggregate/parameter-aggregate.ts
@@ -1,8 +1,36 @@
-import { Parameter } from "@/core/telemetry/entities/parameter"
-import { Station } from "@/core/telemetry/entities/station"
+import { Aggregate } from "@/core/global/domain/abstracts/aggregate";
+import { Logical, Timestamp } from "@/core/global/domain/structures";
+import { Parameter } from "@/core/telemetry/entities/parameter";
+import { ParameterAggregateDto } from "../dtos/parameteraggregate.dto";
 
-export type ParameterAggregate = {
-    id: string,
-    station: Station,
-    parameter: Parameter
+type ParameterAggregateProps = {
+    isActive: Logical
+    createdAt: Timestamp
+    updatedAt?: Timestamp
+}
+
+export class ParameterAggregate extends Aggregate<ParameterAggregateProps, Parameter>{
+    static create(dto: ParameterAggregateDto){
+        return new ParameterAggregate(
+            {
+                isActive: Logical.create(dto.isActive),
+                createdAt: Timestamp.create(dto.createdAt)
+            },
+            Parameter.create(dto.parameter),
+            "Parameter",
+            dto.id
+        )
+    }
+
+     get isActive(): Logical{
+        return this.props.isActive
+    }
+
+    get createdAt(): Timestamp {
+        return this.props.createdAt
+    }
+
+    get updatedAt(): Timestamp | undefined{
+        return this.props.updatedAt
+    }
 }
--- src/core/alerting/dtos/parameteraggregate.dto.ts
+++ src/core/alerting/dtos/parameteraggregate.dto.ts
@@ -3,4 +3,7 @@ import { ParameterDto } from "@/core/telemetry/dtos/parameter-dto"
 export type ParameterAggregateDto = {
     id: string
     parameter: ParameterDto
+    isActive: boolean
+    createdAt: Date
+    updatedAt?: Date
 }

95e4fbeea9bae5ad0cc2bf9a2573fec527310c02
kaufon <kauanfvale@gmail.com>
2025-09-17 07:44:54 -0300

    fix: fix error

--- src/core/telemetry/domain/errors/station-already-activated-error.ts
+++ src/core/telemetry/domain/errors/station-already-activated-error.ts
@@ -1,4 +1,6 @@
-export class StationAlreadyActivatedError extends Error {
+import { ConflictError } from "@/core/global/domain/errors"
+
+export class StationAlreadyActivatedError extends ConflictError {
   constructor() {
     super('Estação já está ativada')
     this.name = 'StationAlreadyActivatedError'

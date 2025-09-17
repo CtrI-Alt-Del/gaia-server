2374707c78af54950dddef9ee986106ac0e2ee94
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-17 00:26:16 -0300

    refactor: extend UsersListingParams to include optional name filter

--- src/core/membership/domain/types/users-listing-params.ts
+++ src/core/membership/domain/types/users-listing-params.ts
@@ -1,4 +1,5 @@
+import { Text } from '@/core/global/domain/structures'
 import { CursorPaginationParams } from '@/core/global/domain/types'
 import { ListingParams } from '@/core/global/domain/types/listing-params'
 
-export type UsersListingParams = ListingParams & CursorPaginationParams
+export type UsersListingParams = ListingParams & CursorPaginationParams & { name?: Text }

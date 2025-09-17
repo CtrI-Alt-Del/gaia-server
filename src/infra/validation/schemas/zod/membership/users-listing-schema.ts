f35196e161586034827a6b9e42c06c99c67a4b5a
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-17 00:26:23 -0300

    refactor: make name filter optional in usersListingSchema

--- src/infra/validation/schemas/zod/membership/users-listing-schema.ts
+++ src/infra/validation/schemas/zod/membership/users-listing-schema.ts
@@ -3,6 +3,7 @@ import z from 'zod'
 import { plusIntegerSchema, statusSchema, stringSchema } from '../global'
 
 export const usersListingSchema = z.object({
+  name: stringSchema.optional(),
   status: statusSchema.optional().default('all'),
   nextCursor: stringSchema.optional(),
   previousCursor: stringSchema.optional(),

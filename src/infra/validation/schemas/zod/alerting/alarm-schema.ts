99ab252b87055d24fe2d88d48ca0176f324eddab
CaiquePastelDePao <caiquepericles.23@gmail.com>
2025-09-20 20:20:40 -0300

    feat: created alarm schema

--- src/infra/validation/schemas/zod/alerting/alarm-schema.ts
+++ src/infra/validation/schemas/zod/alerting/alarm-schema.ts
@@ -1,5 +1,12 @@
 import z from "zod";
+import { booleanSchema, stringSchema } from "../global";
+import { measurementSchema } from "./measurement-schema";
+import { alarmRuleSchema } from "./alarm-rule-schema";
 
 export const alarmSchema = z.object({
-    
+    message: stringSchema,
+    measurement: measurementSchema,
+    rule: alarmRuleSchema,
+    level: stringSchema,
+    isActive: booleanSchema.optional()
 })

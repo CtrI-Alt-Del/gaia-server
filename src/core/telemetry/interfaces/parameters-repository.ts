8f631b3eddd658748c5f5bb818223ebc49c2eb7c
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-20 20:59:18 -0300

    feat: add ParametersRepository and StationsRepository interfaces for telemetry data management

---
+++ src/core/telemetry/interfaces/parameters-repository.ts
@@ -0,0 +1,12 @@
+import { CursorPagination, Id } from '@/core/global/domain/structures'
+import { ParametersListParams } from '@/core/global/types'
+import { Parameter } from '@/core/telemetry/domain/entities/parameter'
+
+export interface ParametersRepository {
+  add(parameter: Parameter): Promise<void>
+  findById(id: Id): Promise<Parameter | null>
+  findMany(params: ParametersListParams): Promise<CursorPagination<Parameter>>
+  findManyByIds(ids:Id[]): Promise<Parameter[]>
+  replace(parameter: Parameter): Promise<void>
+  deleteById(id: Id): Promise<void>
+}

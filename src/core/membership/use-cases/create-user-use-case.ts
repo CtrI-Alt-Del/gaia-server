0af2a3c3e92026f82d3d762e1c4f642bebe931c0
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-09-20 21:03:13 -0300

    feat: enhance CreateUserUseCase to publish UserCreatedEvent and update tests for event handling

--- src/core/membership/use-cases/create-user-use-case.ts
+++ src/core/membership/use-cases/create-user-use-case.ts
@@ -1,11 +1,16 @@
 import { Text } from '@/core/global/domain/structures'
-import { UseCase, UsersRepository } from '@/core/global/interfaces'
+import { EventBroker, UseCase } from '@/core/global/interfaces'
+import { UsersRepository } from '@/core/membership/interfaces'
 import { UserDto } from '../domain/entities/dtos'
 import { User } from '../domain/entities/user'
 import { EmailAlreadyInUseError, OwnerCreationNotAllowed } from '../domain/errors'
+import { UserCreatedEvent } from '../domain/events'
 
 export class CreateUserUseCase implements UseCase<UserDto, UserDto> {
-  constructor(private readonly repository: UsersRepository) {}
+  constructor(
+    private readonly repository: UsersRepository,
+    private readonly eventBroker: EventBroker,
+  ) {}
 
   async execute(userDto: UserDto): Promise<UserDto> {
     const user = User.create(userDto)
@@ -17,6 +22,12 @@ export class CreateUserUseCase implements UseCase<UserDto, UserDto> {
     await this.findUserByEmail(user.email)
 
     await this.repository.add(user)
+
+    const event = new UserCreatedEvent({
+      userId: user.id.value,
+      userEmail: user.email.value,
+    })
+    await this.eventBroker.publish(event)
 
     return user.dto
   }

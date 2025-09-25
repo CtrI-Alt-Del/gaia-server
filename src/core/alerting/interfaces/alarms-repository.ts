c8d729227ae7c7b30bbd43990e133d07b2d0e48f
CaiquePastelDePao <caiquepericles.23@gmail.com>
2025-09-25 10:32:11 -0300

    feat: created find by id repository in alarm

--- src/core/alerting/interfaces/alarms-repository.ts
+++ src/core/alerting/interfaces/alarms-repository.ts
@@ -1,5 +1,7 @@
 import { Alarm } from "@/core/alerting/domain/entities/alarm";
+import { Id } from "@/core/global/domain/structures";
 
 export interface AlarmsRepository {
     add(alarm: Alarm): Promise<void>
+    findById(id: Id): Promise<Alarm | null>
 }
--- src/infra/database/prisma/repositories/prisma-alarms-repository.ts
+++ src/infra/database/prisma/repositories/prisma-alarms-repository.ts
@@ -5,6 +5,7 @@ import type { AlarmsRepository } from '@/core/global/interfaces'
 import { Prisma } from '../client'
 import { Alarm } from '@/core/alerting/domain/entities/alarm'
 import { PrismaAlarmMapper } from '../mappers'
+import { Id } from '@/core/global/domain/structures'
 
 @Injectable()
 export class PrismaAlarmsRepository implements AlarmsRepository {
@@ -12,5 +13,17 @@ export class PrismaAlarmsRepository implements AlarmsRepository {
   async add(alarm: Alarm): Promise<void> {
     const prismaAlarm = PrismaAlarmMapper.toPrisma(alarm)
     await this.prisma.alarm.create({data: prismaAlarm})
+  }
+
+  async findById(id: Id): Promise<Alarm | null> {
+    const prismaAlarm = await this.prisma.alarm.findUnique({
+      where: {id: id.value}
+    })
+
+    if (!prismaAlarm) {
+      return null
+    }
+
+    return PrismaAlarmMapper.toEntity(prismaAlarm)
   }
 }

cf44cc92c012865ce0682edc98626bf959290fac
Caique Péricles Silva <110677265+PasteldePaodeCoxinha@users.noreply.github.com>
2025-09-26 10:27:28 -0300

    Gaia 28 server endpoint de listar regras de alarmes (#35)
    * feat: created alarm listing params
    
    * feat: added findmany methodo in prisma alarm repository
    
    * feat: created the list alarm use case
    
    * feat: created list alarm controller

--- src/core/global/interfaces/alarms-repository.ts
+++ src/core/global/interfaces/alarms-repository.ts
@@ -1,5 +1,8 @@
 import { Alarm } from "@/core/alerting/domain/entities/alarm";
+import { AlarmListingParams } from "../types/alarm-list-params";
+import { CursorPagination } from "../domain/structures";
 
 export interface AlarmsRepository {
     add(alarm: Alarm): Promise<void>
+    findMany(params: AlarmListingParams): Promise<CursorPagination<Alarm>>
 }
--- src/infra/database/prisma/repositories/prisma-alarms-repository.ts
+++ src/infra/database/prisma/repositories/prisma-alarms-repository.ts
@@ -5,12 +5,36 @@ import type { AlarmsRepository } from '@/core/global/interfaces'
 import { Prisma } from '../client'
 import { Alarm } from '@/core/alerting/domain/entities/alarm'
 import { PrismaAlarmMapper } from '../mappers'
+import { AlarmListingParams } from '@/core/global/types/alarm-list-params'
+import { CursorPagination } from '@/core/global/domain/structures'
+import { PrismaRepository } from './prisma-repository'
 
 @Injectable()
-export class PrismaAlarmsRepository implements AlarmsRepository {
-  constructor(private readonly prisma: Prisma) {}
+export class PrismaAlarmsRepository extends PrismaRepository implements AlarmsRepository {
+
   async add(alarm: Alarm): Promise<void> {
     const prismaAlarm = PrismaAlarmMapper.toPrisma(alarm)
     await this.prisma.alarm.create({data: prismaAlarm})
   }
-}
+  
+  async findMany({
+      nextCursor,
+      previousCursor,
+      pageSize,
+      status,
+    }: AlarmListingParams): Promise<CursorPagination<Alarm>> {
+      const whereClause = status?.isAll.isTrue
+        ? undefined
+        : { isActive: status?.isActive.isTrue }
+  
+      const query = this.createPaginationQuery(this.prisma.alarm, whereClause)
+  
+      const result = await this.paginateWithCursor<any>(query, {
+        nextCursor,
+        previousCursor,
+        pageSize,
+      })
+  
+      return result.map(PrismaAlarmMapper.toEntity)
+    }
+  }

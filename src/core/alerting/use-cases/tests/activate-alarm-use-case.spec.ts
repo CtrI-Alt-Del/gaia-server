e2c56b02d338af56964c70f6fc7671b2053c82ac
CaiquePastelDePao <caiquepericles.23@gmail.com>
2025-09-26 23:14:36 -0300

    feat: created the test to activate alarm use case

---
+++ src/core/alerting/use-cases/tests/activate-alarm-use-case.spec.ts
@@ -0,0 +1,44 @@
+import { mock, MockProxy } from "vitest-mock-extended"
+import { AlarmsRepository } from "../../interfaces"
+import { ActivateAlarmUseCase } from "../activate-alarm-use-case"
+import { AlarmFaker } from "../../domain/entities/fakers/alarm-faker"
+import { AlarmNotFoundError } from "@/core/telemetry/domain/errors/alarm-not-found-error"
+import { AlarmAlreadyActivatedError } from "@/core/telemetry/domain/errors/alarm-already-activated-error"
+
+describe('ActivateAlarmUseCase', () => {
+  let repository: MockProxy<AlarmsRepository>
+  let useCase: ActivateAlarmUseCase
+
+  beforeEach(() => {
+    repository = mock<AlarmsRepository>()
+    useCase = new ActivateAlarmUseCase(repository)
+  })
+
+  it('should throw an error if the alarm is not found', async () => {
+    const alarm = AlarmFaker.fake({ isActive: false })
+    repository.findById.mockResolvedValue(null)
+
+    await expect(useCase.execute({ id: alarm.id.value })).rejects.toThrow(
+      AlarmNotFoundError,
+    )
+  })
+
+  it('should throw an error if the alarm is already activated', async () => {
+    const alarm = AlarmFaker.fake({ isActive: true })
+    repository.findById.mockResolvedValue(alarm)
+
+    await expect(useCase.execute({ id: alarm.id.value })).rejects.toThrow(
+      AlarmAlreadyActivatedError,
+    )
+  })
+
+  it('should activate the alarm and replace it in the repository', async () => {
+    const alarm = AlarmFaker.fake({ isActive: false })
+    repository.findById.mockResolvedValue(alarm)
+
+    await useCase.execute({ id: alarm.id.value })
+
+    expect(repository.replace).toHaveBeenCalledWith(alarm)
+    expect(alarm.isActive.isTrue).toBe(true)
+  })
+})

aeb532f4102dc574e9f3ade4483a88d8e576a5dd
Caique Péricles Silva <110677265+PasteldePaodeCoxinha@users.noreply.github.com>
2025-09-30 21:09:22 -0300

    merge: pull request #48

--- src/core/alerting/domain/entities/alarm.ts
+++ src/core/alerting/domain/entities/alarm.ts
@@ -64,6 +64,9 @@ export class Alarm extends Entity<AlarmProps> {
     if (partialDto.rule !== undefined) {
       this.props.rule = AlertRule.create(partialDto.rule)
     }
+    if (partialDto.parameter !== undefined) {
+      this.props.parameter = ParameterAggregate.create(partialDto.parameter)
+    }
     this.refreshLastUpdate()
     return this
   }

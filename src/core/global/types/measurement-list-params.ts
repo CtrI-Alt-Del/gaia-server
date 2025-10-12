8cba5273a49f359f3dc3f5f910056e6add92ca67
CaiquePastelDePao <caiquepericles.23@gmail.com>
2025-10-12 19:28:10 -0300

    feat: #GAIA-127 created find many method in prisma measure repository

--- src/core/global/types/measurement-list-params.ts
+++ src/core/global/types/measurement-list-params.ts
@@ -1,7 +1,7 @@
 import { Text, Timestamp } from "../domain/structures";
 import { CursorPaginationParams, ListingParams } from "../domain/types";
 
-export type MeasurementListParams = (ListingParams & CursorPaginationParams) & {
+export type MeasurementListParams = ListingParams & CursorPaginationParams & {
     stationName?: Text,
     parameterName?: Text,
     date?: Timestamp

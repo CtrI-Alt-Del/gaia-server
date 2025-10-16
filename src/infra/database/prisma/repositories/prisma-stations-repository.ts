f815456528be4d691e6ba0671948d9c7fd325a47
CaiquePastelDePao <caiquepericles.23@gmail.com>
2025-10-16 20:00:46 -0300

    feat: #GAIA-129 added find many by four coords method in prisma stations repo

--- src/infra/database/prisma/repositories/prisma-stations-repository.ts
+++ src/infra/database/prisma/repositories/prisma-stations-repository.ts
@@ -7,6 +7,7 @@ import { CursorPagination, Id } from '@/core/global/domain/structures'
 import { Station } from '@/core/telemetry/domain/entities/station'
 import { PrismaStationMapper } from '@/infra/database/prisma/mappers'
 import { StationsListingParams } from '@/core/global/types/stations-list-params'
+import { StationFourCoordsParams } from '@/core/global/types/station-four-coords-params'
 
 @Injectable()
 export class PrismaStationsRepository
@@ -98,5 +99,20 @@ export class PrismaStationsRepository
     })
 
     return result.map(PrismaStationMapper.toEntity)
+  }
+
+  async findManyByFourCoords(coords: StationFourCoordsParams): Promise<Station[]> {
+    const prismaStations = await this.prisma.station.findMany({
+      where:{
+        AND: [
+          {latitude: {lt: Math.max(coords.lat1, coords.lat2)}},
+          {latitude: {gt: Math.min(coords.lat1, coords.lat2)}},
+          {longitude: {lt: Math.max(coords.long1, coords.long2)}},
+          {longitude: {gt: Math.min(coords.long1, coords.long2)}},
+        ]
+      }
+    })
+
+    return await prismaStations.map(PrismaStationMapper.toEntity)
   }
 }

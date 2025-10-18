da77c628ba2bcd717cb7d5c950cfa9866b25e522
Caique Péricles Silva <110677265+PasteldePaodeCoxinha@users.noreply.github.com>
2025-10-18 19:58:33 -0300

    Gaia 129 server endpoint mapa de estações por bounding box lat long (#51)
    * feat: #GAIA-129 added find many by four coords method in prisma stations repo
    
    * feat: #GAIA-129 created list station by coords controller
    
    * fix: #GAIA-129 changed the endpoint from list station bycoords

---
+++ src/core/global/types/station-four-coords-params.ts
@@ -0,0 +1,6 @@
+export type StationFourCoordsParams = {
+    lat1: number,
+    long1: number,
+    lat2: number,
+    long2:number
+}
--- src/core/telemetry/interfaces/stations-repository.ts
+++ src/core/telemetry/interfaces/stations-repository.ts
@@ -1,4 +1,5 @@
 import { CursorPagination, Id } from '@/core/global/domain/structures'
+import { StationFourCoordsParams } from '@/core/global/types/station-four-coords-params'
 import { StationsListingParams } from '@/core/global/types/stations-list-params'
 import { Station } from '@/core/telemetry/domain/entities/station'
 
@@ -8,4 +9,5 @@ export interface StationsRepository {
   findById(id: Id): Promise<Station | null>
   findMany(params: StationsListingParams): Promise<CursorPagination<Station>>
   replace(station: Station, parametersIds: Id[]): Promise<void>
+  findManyByFourCoords(coords: StationFourCoordsParams): Promise<Station[]>
 }

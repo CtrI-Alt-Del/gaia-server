da6d5245f6c2c427fcaf24e43fdba33782fe4713
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-19 16:39:59 -0300

    feat: gaia-142 integrate Bull and Schedule modules for enhanced job processing

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common'
+import { BullModule } from '@nestjs/bull'
 import { ConfigModule } from '@nestjs/config'
+import { ScheduleModule } from '@nestjs/schedule'
 import { APP_GUARD } from '@nestjs/core'
 
 import { envSchema } from '@/infra/provision/env/env'
 import { RestModule } from '@/infra/rest/rest.module'
 import { EnvProviderModule } from './provision/env/env-provider.module'
 import { AuthModule } from './auth/auth.module'
-import { ClerkAuthGuard } from './auth/guards/clerk-auth.guard'
 import { EventEmitterModule } from '@nestjs/event-emitter'
-import { QueueModule } from './queue/queue.module'
-import { ClerkProvider } from './auth/passport/clerk-provider'
+import { EnvProvider } from './provision/env/env-provider'
+import { AuthQueueModule } from './queue/auth/auth-queue.module'
+import { TelemetryQueueModule } from './queue/telemetry/telemetry-queue.module'
+import { AlertingQueueModule } from './queue/alerting/alerting-queue.module'
 
 @Module({
   imports: [
@@ -17,10 +20,32 @@ import { ClerkProvider } from './auth/passport/clerk-provider'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     EventEmitterModule.forRoot(),
+    BullModule.forRootAsync({
+      imports: [EnvProviderModule],
+      inject: [EnvProvider],
+      useFactory: (envProvider: EnvProvider) => ({
+        connection: {
+          host: envProvider.get('REDIS_HOST'),
+          port: envProvider.get('REDIS_PORT'),
+        },
+        defaultJobOptions: {
+          removeOnComplete: 100,
+          removeOnFail: 100,
+          attempts: 3,
+          backoff: {
+            type: 'exponential',
+            delay: 1000,
+          },
+        },
+      }),
+    }),
+    AuthQueueModule,
+    AlertingQueueModule,
+    TelemetryQueueModule,
     EnvProviderModule,
     RestModule,
-    QueueModule,
     AuthModule,
   ],
   // providers: [ Habilita proteção em todas as rotas

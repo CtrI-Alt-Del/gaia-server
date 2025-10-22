0f50e4b7ac8e8e728d70bd0d58f8eb308f4eba19
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-21 21:20:35 -0300

    fix: remove unused zod scherma

--- src/infra/validation/schemas/zod/telemetry/index.ts
+++ src/infra/validation/schemas/zod/telemetry/index.ts
@@ -1,4 +1,3 @@
 export { parameterSchema } from './parameter-schema'
-export { createStationSchema } from './create-station-schema'
 export { updateStationSchema } from './update-station-schema'
 export { updateParameterSchema } from './update-parameter-schema'

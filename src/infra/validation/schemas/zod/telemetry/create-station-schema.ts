e9403e47ecd45a2b8dd5b84f525d79993922d180
JohnPetros <joaopcarvalho.cds@gmail.com>
2025-10-21 21:15:23 -0300

    fix: gaia-119 remove create station schema

--- src/infra/validation/schemas/zod/telemetry/create-station-schema.ts
+++
@@ -1,4 +0,0 @@
-import { stringSchema } from '@/infra/validation/schemas/zod/global'
-import { latitudeSchema } from '@/infra/validation/schemas/zod/telemetry/latitude-schema'
-import { longitudeSchema } from '@/infra/validation/schemas/zod/telemetry/longitude-schema'
-import z from 'zod'
